fba5e5316137b69b090557ccc0f315e5ce459998
Simon Bull <simon.bull.84@googlemail.com>
2017-10-03 11:24:23 +0100

    Move config to read from json

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,37 +1,48 @@
+'use strict';
+
 const fs = require('fs');
-const path = require('path');
+const Path = require('path');
 
-const env = (process.env.NODE_ENV ? process.env.NODE_ENV : 'dev');
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'dev';
+const isDev = env === 'dev';
 
-const hotConfigUrl  = process.env.HOT_CONFIG_URL ? process.env.HOT_CONFIG_URL : 'https://localhost:4432/';
-
-module.exports = {
-  loggerSettings: {
-    levels: {
-      info: 0,
-      ok: 1,
-      error: 2,
-    },
-    colors: {
-      info: 'yellow',
-      ok: 'green',
-      error: 'red',
-    },
-  },
-  hostingEnvironment: {
-    env: env,
-    host: process.env.HOST ? process.env.HOST : 'localhost',
-    port: process.env.PORT ? process.env.PORT : 4432,
-    protocol: env == 'dev' ? 'https' : 'http'
-  },
-  authenticatingServer: {
-    url: process.env.AUTHENTICATING_SERVER_URL
-  },
-  hotConfig: {
-    url: hotConfigUrl.endsWith('/') ? hotConfigUrl.substring(0, hotConfigUrl.length - 1) : hotConfigUrl,
-    token: process.env.HOT_CONFIG_TOKEN ? process.env.HOT_CONFIG_TOKEN : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MDY2NzYyODEsImV4cCI6NDY2MDI3NjI4MSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdCJ9.OOWQK63iIBzLXG7rx424fi10ae816_TRmW9SsR4NcmI'
-  },
-  certificates: {
-    storageRoot: path.resolve('./ssl')
+const getSettingsObject = (settings) => {
+  try {
+    return JSON.parse(settings);
+  } catch (e) {
+    return null;
   }
 };
+
+const getSettingsFromFile = (settingsPath) => {
+  if (fs.existsSync(settingsPath)) {
+    const file = fs.readFileSync(settingsPath, 'utf8');
+    try {
+      return JSON.parse(file);
+    } catch (e) {
+      return null;
+    }
+  }
+  return null;
+};
+
+const fetchConfig = () => {
+  if (process.env.settings) {
+    const settings = process.env.settings;
+    let settingsObject = getSettingsObject(settings);
+    if (settingsObject !== null) {
+      return settingsObject;
+    }
+    const settingsPath = Path.resolve(settings);
+    if (fs.existsSync(settingsPath)) {
+      settingsObject = getSettingsFromFile(settingsPath);
+      if (settingsObject !== null) {
+        return settingsObject;
+      }
+    }
+  }
+
+  throw new Error('Missing configuration');
+};
+
+module.exports = fetchConfig();
